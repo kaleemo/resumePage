787183fad4d5115213d3f12fc7ad1a0de9ee00d5
Mike Gharzai <michael@gharzai.net>
2016-12-19 16:35:37 -0600

    padding

--- mypage/views.py
+++ mypage/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import Http404
+from django.http import HttpResponse, Http404 
 
 def home(request):
 	return render(request, 'headerbody/pagecontent/home.html')
-	
+
 def about(request):
 	return render(request, 'headerbody/pagecontent/about.html')
-	
+
 def contact(request):
 	return render(request, 'headerbody/pagecontent/contact.html')
